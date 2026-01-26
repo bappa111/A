9e719d9291beabe26c485d13cb86ca840df2c3eb
bappa4321 <Bappa121235@gmail.com>
2026-01-26 20:21:46 +0530

    updates

--- B/public/profile.js
+++ B/public/profile.js
@@ -256,6 +256,7 @@ async function updateProfile() {
     const d = await up.json();
     profilePicUrl = d.imageUrl;
   }
+    console.log("PROFILE PIC URL:", profilePicUrl);
 
   const res = await fetch(API + "/api/users/profile", {
     method: "PUT",
